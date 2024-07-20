0423d11da21804fe8a17a1f5b017553360f0727c
dmytromaimesko <dima.maimesko@gmail.com>
2024-07-20 15:26:53 +0300

    added group removing protection

--- src/Model/Work/Entity/Members/Member/MemberRepository.php
+++ src/Model/Work/Entity/Members/Member/MemberRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Model\Work\Entity\Members\Member;
 
 use App\Model\EntityNotFoundException;
+use App\Model\Work\Entity\Members\Group\Id as GroupId;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
@@ -41,5 +42,14 @@ class MemberRepository extends ServiceEntityRepository
     public function add(Member $member): void
     {
         $this->entityManager->persist($member);
+    }
+
+    public function hasByGroup(GroupId $id): bool
+    {
+        return $this->createQueryBuilder('t')
+                ->select('COUNT(t.id)')
+                ->andWhere('t.group = :group')
+                ->setParameter(':group', $id->getValue())
+                ->getQuery()->getSingleScalarResult() > 0;
     }
 }
--- src/Model/Work/UseCase/Members/Group/Remove/Handler.php
+++ src/Model/Work/UseCase/Members/Group/Remove/Handler.php
@@ -7,13 +7,14 @@ namespace App\Model\Work\UseCase\Members\Group\Remove;
 use App\Model\Flusher;
 use App\Model\Work\Entity\Members\Group\GroupRepository;
 use App\Model\Work\Entity\Members\Group\Id;
+use App\Model\Work\Entity\Members\Member\MemberRepository;
 
 class Handler
 {
     private $groups;
     private $flusher;
 
-    public function __construct(GroupRepository $groups, Flusher $flusher)
+    public function __construct(GroupRepository $groups, protected MemberRepository $memberRepository, Flusher $flusher)
     {
         $this->groups = $groups;
         $this->flusher = $flusher;
@@ -22,6 +23,10 @@ class Handler
     public function handle(Command $command): void
     {
         $group = $this->groups->get(new Id($command->id));
+
+        if ($this->memberRepository->hasByGroup($group->getId())) {
+            throw new \DomainException('Group is not empty.');
+        }
 
         $this->groups->remove($group);
 
